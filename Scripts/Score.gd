2d7ae1ab6543a46963a0d3a51b6c1373b8045896
jakub.mirocha <jakub.mirocha@hatimeria.pl>
2018-05-19 17:22:24 +0200

    add global Score script

---
+++ Scripts/Score.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var points = 0
+
+func _ready():
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func add_points(score):
+	points += score
