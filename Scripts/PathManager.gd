4240d339062a92da174c61921418c0ca6a5ab89e
Jakub Duzyk <jakub.duzyk@lumesse.com>
2018-05-19 16:06:39 +0200

    Added additional paths

--- Scripts/PathManager.gd
+++ Scripts/PathManager.gd
@@ -2,7 +2,7 @@ extends Node
 
 # EXPORT -------------------------
 var cars = [] 
-export var deployDelay = 2
+export var deployDelay = 1
 onready var carobj = load("res://Scenes/Car.tscn");
 # ONREADY ------------------------
 
