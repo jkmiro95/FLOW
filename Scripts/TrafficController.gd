422d38c38df5ecb34fc008fa96a377761e864ea4
Jakub Duzyk <jakub.duzyk@lumesse.com>
2018-05-20 09:13:40 +0200

    Refactor

--- Scripts/TrafficController.gd
+++ Scripts/TrafficController.gd
@@ -1,23 +1,13 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 var blockCars = false
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
-	
-		
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
+
 func _on_Area2D2_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
 		blockCars=!blockCars
 		print("blockCars "+name)
 		print(blockCars)
-	pass # replace with function body
-
+	pass 
